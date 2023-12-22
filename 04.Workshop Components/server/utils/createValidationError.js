2141730bfea4d613b23c3364c3a48769086a6038
Mihail Donchev <mihaildonchev88@abv.bg>
2023-12-22 23:27:26 +0200

    Create createValidationError.js

---
+++ 04.Workshop Components/server/utils/createValidationError.js
@@ -0,0 +1,10 @@
+class ValidationError extends Error {
+    constructor(message, code) {
+      super(message);
+      this.name = 'CustomValidationError';
+      this.code = code || 400;
+    }
+  }
+  
+  module.exports = { ValidationError };
+  
