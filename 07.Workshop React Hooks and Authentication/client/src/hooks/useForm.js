7d89ba4f9234dd74ef4c0675abb50803ee5ca73a
Mihail Donchev <mihaildonchev88@abv.bg>
2023-12-28 19:31:38 +0200

    Create useForm.js

---
+++ 07.Workshop React Hooks and Authentication/client/src/hooks/useForm.js
@@ -0,0 +1,29 @@
+import { useState } from "react";
+
+export const useForm = (initialValues, onSubmitHandler) => {
+
+    const [values, setValues] = useState(initialValues);
+
+    const changeHandler = (event) => {
+        setValues(state => ({ ...state, [event.target.name]: event.target.value }));
+    }
+
+    const onSubmit = (event) => {
+        event.preventDefault();
+
+        onSubmitHandler(values);
+    }
+
+    const changeValues = (newValues) => {
+        //TODO: Validate newValues shape (like initialValues)
+
+        setValues(newValues);
+    }
+
+    return {
+        values,
+        changeHandler,
+        onSubmit,
+        changeValues,
+    }
+}
