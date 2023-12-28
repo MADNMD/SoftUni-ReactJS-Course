d863e4c229042d8943679d08e16a0e331febbf14
Mihail Donchev <mihaildonchev88@abv.bg>
2023-12-28 19:29:49 +0200

    Create commentService.js

---
+++ 07.Workshop React Hooks and Authentication/client/src/services/commentService.js
@@ -0,0 +1,20 @@
+import * as request from './requester';
+
+const baseUrl = 'http://localhost:3030/jsonstore/comments';
+
+export const getAll = async(gameId) => {
+
+    const query = encodeURIComponent(`gameId=${gameId}`);
+
+    const result = await request.get(`${baseUrl}?where=${query}`);
+    const comments = Object.values(result);
+
+    return comments;
+}
+
+export const createComment = async (data) => {
+
+    const result = await request.post(baseUrl, data);
+
+    return result;
+} 
