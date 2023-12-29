116593d2eeeee8c67678a9f9bfc9316c1ab0fd09
Mihail Donchev <mihaildonchev88@abv.bg>
2023-12-29 22:23:49 +0200

    Create CreateGame.jsx

---
+++ 07.Workshop React Hooks and Authentication/client/src/components/CreateGame/CreateGame.jsx
@@ -0,0 +1,77 @@
+import { useContext } from 'react';
+
+import { AuthContext } from '../../contexts/AuthContext';
+import { useForm } from '../../hooks/useForm';
+
+
+export const CreateGame = () => {
+
+    const { onCreateGameSubmit } = useContext(AuthContext);
+    const { values, changeHandler, onSubmit } = useForm({
+        title: '',
+        category: '',
+        maxLevel: '',
+        imageUrl: '',
+        summary: '',
+    }, onCreateGameSubmit)
+
+    return (
+        <section id="create-page" className="auth">
+            <form id="create" method='POST' onSubmit={onSubmit}>
+                <div className="container">
+
+                    <h1>Create Game</h1>
+                    <label htmlFor="leg-title">Legendary title:</label>
+                    <input
+                        type="text"
+                        id="title"
+                        name="title"
+                        placeholder="Enter game title..."
+                        value={values.title}
+                        onChange={changeHandler}
+                    />
+
+                    <label htmlFor="category">Category:</label>
+                    <input
+                        type="text"
+                        id="category"
+                        name="category"
+                        placeholder="Enter game category..."
+                        value={values.category}
+                        onChange={changeHandler}
+                    />
+
+                    <label htmlFor="levels">MaxLevel:</label>
+                    <input
+                        type="number"
+                        id="maxLevel"
+                        name="maxLevel"
+                        min="1"
+                        placeholder="1"
+                        value={values.maxLevel}
+                        onChange={changeHandler}
+                    />
+
+                    <label htmlFor="game-img">Image:</label>
+                    <input
+                        type="text"
+                        id="imageUrl"
+                        name="imageUrl"
+                        placeholder="Upload a photo..."
+                        value={values.imageUrl}
+                        onChange={changeHandler}
+                    />
+
+                    <label htmlFor="summary">Summary:</label>
+                    <textarea
+                        name="summary"
+                        id="summary"
+                        value={values.summary}
+                        onChange={changeHandler}
+                    ></textarea>
+                    <input className="btn submit" type="submit" value="Create Game" />
+                </div>
+            </form>
+        </section>
+    )
+}
