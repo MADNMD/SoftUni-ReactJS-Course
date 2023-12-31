bb6c0fcc4c11e2c920113f29ffbf2199a4471df7
Mihail Donchev <mihaildonchev88@abv.bg>
2023-12-31 17:16:57 +0200

    Create gameService.js

---
+++ 08.Advanced Techniques/client/src/services/gameService.js
@@ -0,0 +1,52 @@
+import { requestFactory } from './requester';
+
+const baseUrl = 'http://localhost:3030/data/games';
+
+export const gameServiceFactory = (token) => {
+
+    const request = requestFactory(token);
+
+    const getAll = async () => {
+
+        const result = await request.get(baseUrl);
+        const games = Object.values(result);
+
+        return games;
+    };
+
+    const getOne = async (gameId) => {
+
+        const result = await request.get(`${baseUrl}/${gameId}`);
+
+        return result;
+    };
+
+    const editOne = async (gameId, gameData) => {
+
+        const result = await request.put(`${baseUrl}/${gameId}`, gameData);
+
+        return result;
+    }
+
+    const deleteOne = async (gameId) => {
+
+        const result = await request.delete(`${baseUrl}/${gameId}`);
+
+        return result;
+    }
+
+    const createGame = async (gameData) => {
+
+        const result = await request.post(baseUrl, gameData);
+
+        return result;
+    };
+
+    return {
+        getAll,
+        getOne,
+        editOne,
+        deleteOne,
+        createGame,
+    }
+}
