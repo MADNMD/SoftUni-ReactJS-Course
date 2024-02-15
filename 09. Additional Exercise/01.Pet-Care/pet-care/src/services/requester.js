978dc4e5709f034c68924d57c79bfcb577219025
Mihail Donchev <mihaildonchev88@abv.bg>
2024-02-15 19:12:48 +0200

    Create requester.js

---
+++ 09. Additional Exercise/01.Pet-Care/pet-care/src/services/requester.js
@@ -0,0 +1,65 @@
+const baseUrl = 'http://localhost:3030';
+
+async function request(url, option) {
+
+    try {
+
+        const response = await fetch(baseUrl + url, option);
+
+        if (response.ok !== true) {
+            const err = await response.json();
+            throw new Error(err.message);
+        }
+
+        if (response.status === 204) {
+            return response;
+        } else {
+            return response.json();
+        }
+
+    } catch (error) {
+        alert(error.message)
+        throw error;
+    }
+}
+
+function createOptions(method = 'get', data) {
+
+    const option = {
+        method,
+        headers: {},
+    }
+
+    if (data !== undefined) {
+        option.headers['Content-type'] = 'Application/json';
+        option.body = JSON.stringify(data);
+    }
+
+    const userData = localStorage.getItem('userData');
+    
+    if (userData) {
+        const parsedUserData = JSON.parse(userData);
+        const token = parsedUserData.accessToken;
+
+        if (token) {
+            option.headers['X-Authorization'] = token;
+        }
+    }
+    return option
+}
+
+export async function get(url) {
+    return request(url, createOptions());
+}
+
+export async function post(url, data) {
+    return request(url, createOptions('post', data));
+}
+
+export async function put(url, data) {
+    return request(url, createOptions('put', data));
+}
+
+export async function del(url) {
+    return request(url, createOptions('delete'));
+}
