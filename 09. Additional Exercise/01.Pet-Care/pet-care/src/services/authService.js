9603fb432ad3f716f83b281dab94adfcc7fbd81c
Mihail Donchev <mihaildonchev88@abv.bg>
2024-02-15 19:14:30 +0200

    Create authService.js

---
+++ 09. Additional Exercise/01.Pet-Care/pet-care/src/services/authService.js
@@ -0,0 +1,28 @@
+import * as requester from './requester';
+
+
+export const login = async (userData) => {
+
+    try {
+        const result = await requester.post('/users/login', userData);
+        return result;
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
+}
+
+export const register = async (userData) => {
+
+    try {
+        const registerUser = await requester.post('/users/register', userData);
+        return registerUser;
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
+}
+
+export const logout = async () => {
+    await requester.get('/users/logout');
+}
