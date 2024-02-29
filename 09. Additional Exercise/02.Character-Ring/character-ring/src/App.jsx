258f058ad2d408a930ec01baa29ef885e2e5f5bd
Mihail Donchev <mihaildonchev88@abv.bg>
2024-02-29 15:11:35 +0200

    Update App.jsx

--- 09. Additional Exercise/02.Character-Ring/character-ring/src/App.jsx
+++ 09. Additional Exercise/02.Character-Ring/character-ring/src/App.jsx
@@ -1,34 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Routes, Route } from "react-router-dom";
+
+import { AuthProvider } from "./context/AuthContext";
+
+import { CreatePage } from "./components/CreatePage";
+import { Dashboard } from "./components/Dashboard";
+import { DetailsPage } from "./components/DetailsPage";
+import { EditPage } from "./components/EditPage";
+import { Footer } from "./components/Footer";
+import { Home } from "./components/Home";
+import { Login } from "./components/Login";
+import { Navigation } from "./components/Navigation";
+import { Register } from "./components/Register";
+import { Logout } from "./components/Logout";
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <AuthProvider>
+      <>
+        <div className="wrapper">
+          <Navigation />
+          <main>
+            <Routes>
+              <Route path='/' element={<Home />} />
+              <Route path='/catalog' element={<Dashboard />} />
+              <Route path='/create-page' element={<CreatePage />} />
+              <Route path='/edit-page/:charId' element={<EditPage />} />
+              <Route path='/details-page/:charId' element={<DetailsPage />} />
+              <Route path='/user/login' element={<Login />} />
+              <Route path='/user/register' element={<Register />} />
+              <Route path="/user/logout" element={<Logout />} />
+            </Routes>
+          </main>
+        </div>
+        <Footer />
+      </>
+    </AuthProvider>
   )
 }
 
