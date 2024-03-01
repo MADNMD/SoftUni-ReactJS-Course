a2bf4d3b168cc9ab7fb2a4b6040820e69899ba86
Mihail Donchev <mihaildonchev88@abv.bg>
2024-03-01 14:24:09 +0200

    Create requester.js

---
+++ 09. Additional Exercise/02.Character-Ring/character-ring/src/services/requester.js
@@ -0,0 +1,63 @@
+const baseURL = 'http://localhost:3030';
+
+async function request(url, option) {
+
+    try {
+        const response = await fetch(baseURL + url, option);
+
+        if (response.ok !== true) {
+            const err = await response.json();
+            throw new Error(err.message);
+        }
+
+        if (response.status === 204) {
+            return response;
+        } else {
+            return response.json();
+        }
+    } catch (error) {
+        alert(error.message);
+        throw error;
+    }
+}
+
+function createOption(method = 'get', data) {
+
+    const option = {
+        method,
+        headers: {}
+    }
+
+    if (data !== undefined) {
+        option.headers['Content-type'] = 'Application/json';
+        option.body = JSON.stringify(data);
+    }
+
+    const userData = localStorage.getItem('userData');
+
+    if (userData) {
+        const parsedUserData = JSON.parse(userData);
+        const token = parsedUserData.accessToken;
+
+        if (token) {
+            option.headers['X-Authorization'] = token;
+        }
+    }
+    return option;
+}
+
+export async function get(url) {
+    return request(url, createOption());
+}
+
+export async function post(url, data) {
+    return request(url, createOption('post', data));
+}
+
+export async function put(url, data) {
+    return request(url, createOption('put', data));
+}
+
+export async function del(url) {
+    return request(url, createOption('delete'));
+}
