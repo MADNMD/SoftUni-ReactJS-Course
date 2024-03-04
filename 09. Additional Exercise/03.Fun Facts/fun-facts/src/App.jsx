ec9994b399552570353a9e3bf4ddc9a8d3a85249
Mihail Donchev <mihaildonchev88@abv.bg>
2024-03-04 14:52:10 +0200

    Update App.jsx

--- 09. Additional Exercise/03.Fun Facts/fun-facts/src/App.jsx
+++ 09. Additional Exercise/03.Fun Facts/fun-facts/src/App.jsx
@@ -1,34 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Routes, Route } from 'react-router-dom';
+
+import { Navigation } from "./components/Navigation";
+import { Footer } from './components/Footer';
+import { Login } from "./components/Login/";
+import { Logout } from './components/Logout';
+import { Register } from "./components/Register";
+import { Catalog } from "./components/Catalog";
+import { CreateFact } from "./components/CreateFact";
+import { EditFact } from "./components/EditFact";
+import { DetailsFact } from "./components/DetailsFact";
+import { Home } from './components/Home';
+import { AuthProvider } from './context/AuthContext';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <AuthProvider>
+      <div id="wrapper">
+        <Navigation />
+        <main>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/user/login' element={<Login />} />
+            <Route path='/user/register' element={<Register />} />
+            <Route path='/user/logout' element={<Logout />} />
+            <Route path='/catalog' element={<Catalog />} />
+            <Route path='/create-fact' element={<CreateFact />} />
+            <Route path='/edit-fact/:factId' element={<EditFact />} />
+            <Route path='/details-fact/:factId' element={<DetailsFact />} />
+          </Routes>
+        </main>
+        <Footer />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </AuthProvider>
   )
 }
 
