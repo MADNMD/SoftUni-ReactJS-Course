650f464e774892dacfa571a2d509cb8877b2df29
Mihail Donchev <mihaildonchev88@abv.bg>
2024-03-18 19:22:11 +0200

    Create Home.jsx

---
+++ 09. Additional Exercise/05.Samurider/samurider/src/components/Home/Home.jsx
@@ -0,0 +1,10 @@
+export const Home = () => {
+    return (
+        <section id="home">
+            <h1>
+                Welcome to <span>Samurider</span> moto market, your premier destination for Japanese motorcycles.</h1>
+            <img
+                src="./public/images/motorcycle.png" alt="home" />
+        </section>
+    )
+}
