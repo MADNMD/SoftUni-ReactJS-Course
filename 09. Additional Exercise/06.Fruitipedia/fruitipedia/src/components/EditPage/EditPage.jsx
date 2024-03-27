837ffa6e766226436f691f5f5bfca4b06fe4f998
Mihail Donchev <mihaildonchev88@abv.bg>
2024-03-27 15:12:17 +0200

    Create EditPage.jsx

---
+++ 09. Additional Exercise/06.Fruitipedia/fruitipedia/src/components/EditPage/EditPage.jsx
@@ -0,0 +1,128 @@
+import { useFormik } from 'formik';
+import { useEffect } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+
+import * as fruitService from '../../services/fruitService';
+
+export const EditPage = () => {
+
+    const { fruitId } = useParams();
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        fruitService.getOneFruit(fruitId)
+            .then(fruitData => {
+                formik.setValues({
+                    name: fruitData.name || '',
+                    imageUrl: fruitData.imageUrl || '',
+                    description: fruitData.description || '',
+                    nutrition: fruitData.nutrition || ''
+                })
+            })
+    }, [fruitId]);
+
+    const handleEditFruit = (fruitData) => {
+
+        try {
+            fruitService.editFruit(fruitId, fruitData);
+
+            navigate(`/details-page/${fruitId}`);
+        } catch (error) {
+            console.log(error);
+            throw error;
+        }
+    }
+
+    const initialValues = {
+        name: '',
+        imageUrl: '',
+        description: '',
+        nutrition: '',
+    }
+
+    const onSubmit = (values) => {
+        handleEditFruit(values);
+    }
+
+    const validate = (values) => {
+        const errors = {}
+
+        if (!values.name) {
+            errors.name = 'Name is required!'
+        }
+
+        if (!values.imageUrl) {
+            errors.imageUrl = 'Image url is required!'
+        } else if (!/^https?:\/\//.test(values.imageUrl)) {
+            errors.imageUrl = 'The car image is required and should start with http:// or https://!';
+        }
+
+        if (!values.description) {
+            errors.description = 'Description is required!'
+        }
+
+        if (!values.nutrition) {
+            errors.nutrition = 'Nutrition is required!'
+        }
+        return errors;
+    }
+
+    const formik = useFormik({
+        initialValues,
+        onSubmit,
+        validate,
+    });
+
+    return (
+        <section id="edit">
+            <div className="form">
+                <h2>Edit Fruit</h2>
+                <form className="edit-form" onSubmit={(formik.handleSubmit)}>
+                    <input
+                        type="text"
+                        name="name"
+                        id="name"
+                        placeholder="Fruit Name"
+                        value={formik.values.name}
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
+                    />
+                    {formik.touched.name && formik.errors.name ? <span className='errors-form'>{formik.errors.name}</span> : null}
+                    <input
+                        type="text"
+                        name="imageUrl"
+                        id="Fruit-image"
+                        placeholder="Fruit Image URL"
+                        value={formik.values.imageUrl}
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
+                    />
+                    {formik.touched.imageUrl && formik.errors.imageUrl ? <span className='errors-form'>{formik.errors.imageUrl}</span> : null}
+                    <textarea
+                        id="fruit-description"
+                        name="description"
+                        placeholder="Description"
+                        rows="10"
+                        cols="50"
+                        value={formik.values.description}
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
+                    ></textarea>
+                    {formik.touched.description && formik.errors.description ? <span className='errors-form'>{formik.errors.description}</span> : null}
+                    <textarea
+                        id="fruit-nutrition"
+                        name="nutrition"
+                        placeholder="Nutrition"
+                        rows="10"
+                        cols="50"
+                        value={formik.values.nutrition}
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
+                    ></textarea>
+                    {formik.touched.nutrition && formik.errors.nutrition ? <span className='errors-form'>{formik.errors.nutrition}</span> : null}
+                    <button type="submit">post </button>
+                </form>
+            </div>
+        </section>
+    )
+}
