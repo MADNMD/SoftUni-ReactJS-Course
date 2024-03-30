bb53f911f3784dbfecaad05ede74e04ffef43cb2
Mihail Donchev <mihaildonchev88@abv.bg>
2024-03-30 11:25:31 +0200

    Create Home.jsx

---
+++ 09. Additional Exercise/06.Fruitipedia/fruitipedia/src/components/Home/Home.jsx
@@ -0,0 +1,8 @@
+export const Home = () => {
+    return (
+        <section id="home">
+            <h1>Learn more about your favorite fruits</h1>
+            <img src="./public/images/pexels-pixabay-161559-dImkWBDHz-transformed (1).png" alt="home" />
+        </section>
+    )
+}
