40bfda9de139af12f015d50afaab24707a47d24c
Mihail Donchev <mihaildonchev88@abv.bg>
2024-03-30 11:33:15 +0200

    Create SearchItem.jsx

---
+++ 09. Additional Exercise/06.Fruitipedia/fruitipedia/src/components/Search/SearchItem/SearchItem.jsx
@@ -0,0 +1,14 @@
+import { Link } from 'react-router-dom';
+
+export const SearchItem = ({
+    fruit,
+}) => {
+    return (
+        <div className="fruit">
+            <img src={fruit.imageUrl} alt="example1" />
+            <h3 className="title">{fruit.name}</h3>
+            <p className="description">{fruit.description}</p>
+            <Link className="details-btn" to={`/details-page/${fruit._id}`}>More Info</Link>
+        </div>
+    )
+}
