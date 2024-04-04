2dab8ad676f00ccb7541c8ffd24acb3c579c3e41
Mihail Donchev <mihaildonchev88@abv.bg>
2024-04-04 15:53:50 +0300

    Update App.jsx

--- 09. Additional Exercise/07.Eventer/eventer/src/App.jsx
+++ 09. Additional Exercise/07.Eventer/eventer/src/App.jsx
@@ -1,35 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Routes, Route } from 'react-router-dom'
+
+import { CreatePage } from "./components/Create"
+import { Dashboard } from "./components/Dashboard"
+import { Details } from "./components/Details"
+import { Edit } from "./components/Edit"
+import { Footer } from "./components/Footer"
+import { Home } from "./components/Home"
+import { Login } from "./components/Login"
+import { Navigation } from "./components/Navigation"
+import { Register } from "./components/Register"
+import { AuthProvider } from './contexts/AuthContext'
+import { Logout } from './components/Logout'
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <AuthProvider>
+            <div id="wrapper">
+                <Navigation />
+                <main>
+                    <Routes>
+                        <Route path='/' element={<Home />} />
+                        <Route path='/dashboard' element={<Dashboard />} />
+                        <Route path='/user/register' element={<Register />} />
+                        <Route path='/user/login' element={<Login />} />
+                        <Route path='/create-page' element={<CreatePage />} />
+                        <Route path='/details/:eventId' element={<Details />} />
+                        <Route path='/edit/:eventId' element={<Edit />} />
+                        <Route path='/user/logout' element={<Logout />} />
+                    </Routes>
+                </main>
+            </div>
+            <Footer />
+        </AuthProvider>
+    )
 }
 
 export default App
