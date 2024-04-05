c927a25edbf58a45c51a8aa0260fa50edff32816
Mihail Donchev <mihaildonchev88@abv.bg>
2024-04-05 17:41:03 +0300

    Create Create.jsx

---
+++ 09. Additional Exercise/07.Eventer/eventer/src/components/Create/Create.jsx
@@ -0,0 +1,131 @@
+import { useNavigate } from 'react-router-dom';
+import * as eventService from '../../services/eventService';
+import { useFormik } from 'formik';
+
+export const CreatePage = () => {
+
+    const navigate = useNavigate();
+
+    const handlerCreateSubmit = (eventData) => {
+        eventService.createEvent(eventData)
+            .then(() => {
+                navigate('/dashboard');
+            })
+            .catch(error => {
+                console.log(error)
+            });
+    }
+
+    const initialValues = {
+        name: '',
+        imageUrl: '',
+        category: '',
+        description: '',
+        date: ''
+    }
+
+    const onSubmit = (values) => {
+        handlerCreateSubmit(values);
+    }
+
+    const validate = (values) => {
+        const errors = {}
+
+        if (!values.name) {
+            errors.name = 'Name is required!'
+        }
+
+        if (!values.imageUrl) {
+            errors.imageUrl = 'Image URL is required!'
+        } else if (!/^https?:\/\//.test(values.imageUrl)) {
+            errors.imageUrl = 'The event image is required and should start with http:// or https://!'
+        }
+
+        if (!values.category) {
+            errors.category = 'Category is required!'
+        }
+
+        if (!values.description) {
+            errors.description = 'Description is required!'
+        }
+
+        if (!values.date) {
+            errors.date = 'Date is required!'
+        }
+        return errors;
+    }
+
+    const formik = useFormik({
+        initialValues,
+        onSubmit,
+        validate
+    });
+
+    return (
+        <section id="create">
+            <div className="form">
+                <h2>Add Event</h2>
+                <form className="create-form" onSubmit={formik.handleSubmit}>
+
+                    <input
+                        type="text"
+                        name="name"
+                        id="name"
+                        placeholder="Event"
+                        value={formik.values.name}
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
+                    />
+                    {formik.touched.name && formik.errors.name ? <span className='errors-form'>{formik.errors.name}</span> : null}
+
+                    <input
+                        type="text"
+                        name="imageUrl"
+                        id="event-image"
+                        placeholder="Event Image URL"
+                        value={formik.values.imageUrl}
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
+                    />
+                    {formik.touched.imageUrl && formik.errors.imageUrl ? <span className='errors-form'>{formik.errors.imageUrl}</span> : null}
+
+                    <input
+                        type="text"
+                        name="category"
+                        id="event-category"
+                        placeholder="Category"
+                        value={formik.values.category}
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
+                    />
+                    {formik.touched.category && formik.errors.category ? <span className='errors-form'>{formik.errors.category}</span> : null}
+
+                    <textarea
+                        id="event-description"
+                        name="description"
+                        placeholder="Description"
+                        rows="5"
+                        cols="50"
+                        value={formik.values.description}
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
+                    ></textarea>
+                    {formik.touched.description && formik.errors.description ? <span className='errors-form'>{formik.errors.description}</span> : null}
+
+                    <input
+                        type="text"
+                        name="date"
+                        id="date"
+                        placeholder="When?"
+                        value={formik.values.date}
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
+                    />
+                    {formik.touched.date && formik.errors.date ? <span className='errors-form'>{formik.errors.date}</span> : null}
+
+                    <button type="submit">Add</button>
+                </form>
+            </div>
+        </section>
+    )
+}
