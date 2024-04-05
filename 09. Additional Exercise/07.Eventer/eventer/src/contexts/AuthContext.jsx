e3ad674af4a35fccf75f787b129a168cf7e40035
Mihail Donchev <mihaildonchev88@abv.bg>
2024-04-05 17:40:21 +0300

    Create AuthContext.jsx

---
+++ 09. Additional Exercise/07.Eventer/eventer/src/contexts/AuthContext.jsx
@@ -0,0 +1,80 @@
+import { createContext, useContext } from "react";
+import { useNavigate } from "react-router-dom";
+
+import { useLocaleStorage } from "../hooks/useLocaleStorage";
+import * as authService from '../services/authService';
+
+const AuthContext = createContext();
+
+export const AuthProvider = ({
+    children,
+}) => {
+
+    const [auth, setAuth] = useLocaleStorage('userData', {});
+
+    const navigate = useNavigate();
+
+    const onLoginSubmit = async (userData) => {
+
+        try {
+            const user = await authService.login(userData);
+
+            setAuth(user);
+
+            navigate('/dashboard');
+
+        } catch (error) {
+            console.log(error);
+            throw error;
+        }
+    }
+
+    const onRegisterSubmit = async (userData) => {
+
+        try {
+            const user = await authService.register(userData);
+
+            setAuth(user);
+
+            navigate('/dashboard');
+        } catch (error) {
+            console.log(error);
+            throw error;
+        }
+    }
+
+    const onLogoutSubmit = async () => {
+
+        try {
+           await authService.logout();
+
+            setAuth({});
+
+            navigate('/user/login');
+        } catch (error) {
+            console.log(error);
+            throw error;
+        }
+    }
+
+    const context = {
+        onLoginSubmit,
+        onRegisterSubmit,
+        onLogoutSubmit,
+        userId: auth._id,
+        email: auth.email,
+        token: auth.accessToken,
+        isAuthenticated: !!auth.accessToken,
+    }
+
+    return (
+        <AuthContext.Provider value={context}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
+
+export const useAuthContext = () => {
+    const context = useContext(AuthContext);
+    return context;
+}
