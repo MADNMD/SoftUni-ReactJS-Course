61c69991942a1330ae62d28e1da11e3623554e21
Mihail Donchev <mihaildonchev88@abv.bg>
2024-04-05 17:39:28 +0300

    Create authService.js

---
+++ 09. Additional Exercise/07.Eventer/eventer/src/services/authService.js
@@ -0,0 +1,31 @@
+import * as requester from './requester';
+
+export const login = async (userData) => {
+    try {
+
+        const result = await requester.post('/users/login', userData);
+        return result;
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
+}
+
+export const register = async (userData) => {
+    try {
+        const result = await requester.post('/users/register', userData);
+        return result;
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
+}
+
+export const logout = async () => {
+    try {
+        await requester.get('/users/logout')
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
+}
