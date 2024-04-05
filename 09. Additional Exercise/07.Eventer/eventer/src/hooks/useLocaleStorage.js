f3d78a4b2a04b984633f6245c3360bf6dee6c69b
Mihail Donchev <mihaildonchev88@abv.bg>
2024-04-05 17:39:54 +0300

    Create useLocaleStorage.js

---
+++ 09. Additional Exercise/07.Eventer/eventer/src/hooks/useLocaleStorage.js
@@ -0,0 +1,26 @@
+import { useState } from "react"
+
+export const useLocaleStorage = (key, initialValues) => {
+
+    const [state, setState] = useState(() => {
+
+        const userData = localStorage.getItem(key);
+
+        if (userData) {
+            const getUserData = JSON.parse(userData);
+            return getUserData;
+        }
+        return initialValues;
+    });
+
+    const setLocaleStorage = (values) => {
+
+        setState(values);
+
+        localStorage.setItem(key, JSON.stringify(values));
+    }
+    return [
+        state,
+        setLocaleStorage
+    ]
+}
