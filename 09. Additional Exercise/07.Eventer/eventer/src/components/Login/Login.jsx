67cc3bdbb5bbad878f1c8b85c8b2b6d8201c7085
Mihail Donchev <mihaildonchev88@abv.bg>
2024-04-06 18:51:11 +0300

    Create Login.jsx

---
+++ 09. Additional Exercise/07.Eventer/eventer/src/components/Login/Login.jsx
@@ -0,0 +1,73 @@
+import { Link } from "react-router-dom";
+import { useFormik } from 'formik';
+
+import { useAuthContext } from "../../contexts/AuthContext";
+
+export const Login = () => {
+
+    const { onLoginSubmit } = useAuthContext();
+
+    const initialValues = {
+        email: '',
+        password: '',
+    }
+
+    const onSubmit = (values) => {
+        onLoginSubmit(values);
+    }
+
+    const validate = (values) => {
+
+        const errors = {};
+
+        if (!values.email) {
+            errors.email = 'Email is required!'
+        }
+
+        if (!values.password) {
+            errors.password = 'Password is required!'
+        }
+        return errors;
+    }
+
+    const formik = useFormik({
+        initialValues,
+        onSubmit,
+        validate,
+    });
+
+
+    return (
+        <section id="login">
+            <div className="form">
+                <h2>Login</h2>
+                <form className="login-form" onSubmit={formik.handleSubmit}>
+                    <input
+                        type="text"
+                        name="email"
+                        id="email"
+                        placeholder="email"
+                        value={formik.values.email}
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
+                    />
+                    {formik.touched.email && formik.errors.email ? <span className="errors-form">{formik.errors.email}</span> : null}
+                    <input
+                        type="password"
+                        name="password"
+                        id="password"
+                        placeholder="password"
+                        value={formik.values.password}
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
+                    />
+                    {formik.touched.password && formik.errors.password ? <span className="errors-form">{formik.errors.password}</span> : null}
+                    <button type="submit">login</button>
+                    <p className="message">
+                        Not registered? <Link to="/user/register">Create an account</Link>
+                    </p>
+                </form>
+            </div>
+        </section>
+    )
+}
